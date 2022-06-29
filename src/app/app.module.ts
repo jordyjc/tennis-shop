5fbdbd5d684a57ae6095dd3a22c3322593db8c45
Jordy Jimenez <jjimenezc774.isw@outlook.com>
2022-06-28 23:34:50 -0500

    responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { CarouselShopComponent } from './carousel-shop/carousel-shop.component';
 import { ContentShopComponent } from './content-shop/content-shop.component';
+import { FooterShopComponent } from './footer-shop/footer-shop.component';
 import { HeaderShopComponent } from './header-shop/header-shop.component';
-import { MatIconModule } from '@angular/material/icon'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { NgModule } from '@angular/core';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { CarouselShopComponent } from './carousel-shop/carousel-shop.component';
-import { FooterShopComponent } from './footer-shop/footer-shop.component';
 
 @NgModule({
   declarations: [
@@ -15,15 +16,17 @@ import { FooterShopComponent } from './footer-shop/footer-shop.component';
     HeaderShopComponent,
     ContentShopComponent,
     CarouselShopComponent,
-    FooterShopComponent
+    FooterShopComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
     MatIconModule,
+    MatFormFieldModule,
+    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/header-shop/header-shop.component.ts
+++ src/app/header-shop/header-shop.component.ts
@@ -3,10 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header-shop',
   templateUrl: './header-shop.component.html',
-  styleUrls: ['./header-shop.component.scss']
+  styleUrls: ['./header-shop.component.scss'],
 })
 export class HeaderShopComponent {
   headerDescription = 'Las mejores raquetas para jugar';
-  headerSpan = 'Tennis'
-  buttonLabel = 'acerca de nosotros'
+  headerSpan = 'Tennis';
+  buttonLabel = 'acerca de nosotros';
+  open = false;
+
+  openNav() {
+    this.open = true;
+  }
+
+  closeNav() {
+    this.open = false;
+  }
 }
